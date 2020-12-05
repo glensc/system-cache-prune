5322c3ae44275af022628724631a09b81f27b28d
Elan Ruusamäe <glen@pld-linux.org>
2020-12-05 15:12:43 +0200

    Add config to remove apple:cleanmymac4 files

--- system-cache-prune.sh
+++ system-cache-prune.sh
@@ -24,6 +24,25 @@ prune_subsys() {
 		rm -rf "/Library/Application Support/GarageBand"
 		rm -rf "/Library/Audio/Apple Loops/Apple"
 		;;
+	apple:cleanmymac4)
+		rm -f "$HOME/Library/LaunchAgents/com.macpaw.CleanMyMac4.Updater.plist"
+		rm -f "$HOME/Library/Preferences/com.macpaw.CleanMyMac4.HealthMonitor.plist"
+		rm -f "$HOME/Library/Preferences/com.macpaw.CleanMyMac4.Menu.plist"
+		rm -f "$HOME/Library/Preferences/com.macpaw.CleanMyMac4.plist"
+		rm -rf "$HOME/Library/Caches/com.macpaw.CleanMyMac4"
+		rm -rf "$HOME/Library/Caches/com.macpaw.CleanMyMac4.Updater"
+		rm -rf "$HOME/Library/Group Containers/"*".com.macpaw.CleanMyMac4"
+		rm -rf "$HOME/Library/Logs/CleanMyMac X Menu"
+		rm -rf "$HOME/Library/Logs/com.macpaw.CleanMyMac4"
+		rm -rf "$HOME/Library/WebKit/com.macpaw.CleanMyMac4"
+		rm -rf "$HOME/Library/Application Support/CleanMyMac X"
+		rm -rf "$HOME/Library/Application Support/CleanMyMac X HealthMonitor"
+		rm -rf "$HOME/Library/Application Support/CleanMyMac X Menu"
+		rm -rf "$HOME/Library/Caches/SentryCrash/CleanMyMac X HealthMonitor"
+		rm -rf "$HOME/Library/Caches/SentryCrash/CleanMyMac X Menu"
+		rm -rf "$HOME/Library/Caches/SentryCrash/CleanMyMac X"
+		rm -f "$HOME/Library/Application Support/CleanMyMac X/".???*.*
+		;;
 	composer)
 		composer clear-cache
 		;;
